f2519a83280c7eee57d69027fa41cd56129dc398
Christian Santiago <christian.santiago23@upr.edu>
2018-07-16 21:57:51 -0400

    Added another reusme view, go to 'csantiagoberio.github.io/resume' to see it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes} from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { AuthGuard } from './auth.guard';
+import { ResumeViewComponent } from './resume-view/resume-view.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'about-me', component: AboutMeComponent, canActivate: [AuthGuard]}
+  {path: 'about-me', component: AboutMeComponent, canActivate: [AuthGuard]},
+  {path: 'resume', component: ResumeViewComponent}
 ];
 
 
--- src/app/resume-view/resume-view.component.ts
+++ src/app/resume-view/resume-view.component.ts
@@ -7,9 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumeViewComponent implements OnInit {
 
+  default: Document;
+  show = false;
+
   constructor() { }
 
   ngOnInit() {
+    document.getElementById('default').click();
+  }
+
+  onClick(evt, content, arrow) {
+    let i: number;
+    let tabcontent;
+    let tablinks;
+    let arrows;
+
+    // Hide all the elements of class 'tabcontent'
+    tabcontent = document.getElementsByClassName('tabcontent');
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = 'none';
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName('tablinks');
+    arrows = document.getElementsByClassName('arrow');
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = 'white';
+      arrows[i].style.display = 'none';
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(content).style.display = 'block';
+    document.getElementById(evt).style.backgroundColor = '#4365be';
+    document.getElementById(arrow).style.display = 'block';
+
+  }
+
+  uncollapse(card) {
+    let content;
+
+    content = document.getElementById(card);
+    if (content.style.display === 'none' || content.style.display === '') {
+      content.style.display = 'block';
+    } else {
+      content.style.display = 'none';
+    }
   }
 
 }
